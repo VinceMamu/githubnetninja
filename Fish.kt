6bac19c0526b5b42c114dc87c738bac6f54ce224
VinceMamu <vincemamuyac@gmail.com>
2021-07-28 13:37:43 +0800

    Added all files. git practice

---
+++ Fish.kt
@@ -0,0 +1,14 @@
+import kotlin.math.*
+
+fun main(){
+    println(canAddFish(currentFish = 4.0))
+}
+
+fun canAddFish(tanksize: Double = 10.0, currentFish: Double = 0.0, fishSize: Double = 2.0, hasDecorations: Boolean = true): Boolean{
+    var value: Int = ((if (hasDecorations==true) tanksize*0.8 else tanksize)).toInt()
+    var calculate: Int = ((currentFish * fishSize)).toInt()
+    return when(calculate){
+        in 0..value -> true
+        else -> false
+    }
+}
---
+++ helloworld.kt
@@ -0,0 +1,40 @@
+import java.util.Calendar
+import java.util.Random
+
+fun main(){  
+    println("test")
+
+    var dayToday: Int = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println ( when(dayToday){
+    1 -> "Sun"
+    2 -> "Mon"
+    3 -> "Tues"
+    4 -> "Wed"
+    5 -> "Thurs"
+    6 -> "Fri"
+    7 -> "Sat"
+    else-> "error"})
+    chooseADay()
+
+}
+
+fun timeCheck(){
+    println("Enter your time")
+    var a = readLine()!!
+    println("You inputted $a")
+    var b: Int? = a.toIntOrNull()
+    println( when (b){
+    in 1..12 -> "Morning, Kotlin"
+    in 13..24 -> "Evening, Kotlin"
+    else -> "Error"
+    })
+}
+
+fun chooseADay(){
+    val day = returnDay()
+    println("Today is $day")
+}
+fun returnDay(): String {
+    val week = listOf("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun")
+    return week[Random().nextInt(7)]
+}
