db19629d7960f777b5b67c1e0e5ea49661de4d69
VinceMamu <vincemamuyac@gmail.com>
2021-07-28 13:39:57 +0800

    Changed 10 to 9

--- Fish.kt
+++ Fish.kt
@@ -4,7 +4,7 @@ fun main(){
     println(canAddFish(currentFish = 4.0))
 }
 
-fun canAddFish(tanksize: Double = 10.0, currentFish: Double = 0.0, fishSize: Double = 2.0, hasDecorations: Boolean = true): Boolean{
+fun canAddFish(tanksize: Double = 9.0, currentFish: Double = 0.0, fishSize: Double = 2.0, hasDecorations: Boolean = true): Boolean{
     var value: Int = ((if (hasDecorations==true) tanksize*0.8 else tanksize)).toInt()
     var calculate: Int = ((currentFish * fishSize)).toInt()
     return when(calculate){
